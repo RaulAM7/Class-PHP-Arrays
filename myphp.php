898fd19959f0ea4d2601065e594b657c3b8cec59
RaulAM <raulartilesm@gmail.com>
2024-10-23 11:00:51 +0100

    Update

---
+++ myphp.php
@@ -0,0 +1,51 @@
+<?php
+
+// Clase Arrays
+
+$array_name0  = [];
+
+$array_name0 = ['Raul', 'Artiles'];
+
+$array_name0 = ['Elemento0','Elemento1'];
+
+// Accesing arrays elements
+
+$array_name = [ 'Value1', 'Value2', 'Value3' ];
+
+var_dump( $array_name [0]);
+var_dump( $array_name [1]);
+var_dump( $array_name [2]);
+
+
+// Array methods
+
+$names = [ 'Mario', 'Luisa', 'Andrés' ];
+
+array_push( $names, 'Raul' );
+
+// Splice -> elimina y puede reemplazar
+
+$fruits = ['apple','banana','orange','pear','apricot','cherry'];
+print_r($fruits);
+
+array_splice($fruits, 3, count($fruits), [['new Element0', 'new Element1']]);
+
+print_r($fruits);
+
+
+// Array_slice
+
+$array78 = [
+    0 => 1,
+    1 => 1,
+    2 => 2,
+    3 => 3,
+    4 => 4,
+    5 => 5,
+    6 => 6,
+];
+
+print_r(array_slice($array78, 5, 2, true));
+
+
+?>
